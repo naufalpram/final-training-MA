3d4784f78143906566b3c183d4d2f065ace5e6a3
kyzsu <l0calh34rtz@gmail.com>
2023-03-20 11:42:47 +0700

    feat: update userRoutes with controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express")
-
+const usersController = require("../controllers/usersController")
 const router = express.Router()
 
-router.route("/").get().post().patch().delete()
+router
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.createNewUser)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser)
 
 // router.route('/:id').patch()
 
